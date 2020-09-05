9674a44dc09cb5c23d29ea1f0c193ad525cf5ffa
Fergus MacDermot <fergusmacdermot@gmail.com>
2020-09-05 23:41:30 +0800

    Set up some permissions

---
+++ repositories.tf
@@ -0,0 +1,6 @@
+# Create a new Artifactory local repository called my-local
+resource "artifactory_local_repository" "mvn-local" {
+  key          = "mvn-local"
+  package_type = "maven"
+  repo_layout_ref = "maven-2-default"
+}
