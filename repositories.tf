79fb1f9d9316d1095445912799b3de00887db549
Fergus MacDermot <fergusmacdermot@gmail.com>
2020-09-07 23:08:27 +0800

    Tidy up

--- repositories.tf
+++ repositories.tf
@@ -8,45 +8,41 @@ resource "artifactory_local_repository" "mvn_local" {
 }
 # https://github.com/atlassian/terraform-provider-artifactory/blob/master/website/docs/r/artifactory_local_repository.html.markdown
 resource "artifactory_local_repository" "docker_dev_local" {
-  key             = "docker-dev-local"
-  package_type    = "docker"
-  description     = "Repo for uploading images to for dev (non-prod environments)"
-  notes           = "This repo should be pruned to avoid maintaining old images."
-  dockerApiVersion = "V2"
-  maxUniqueTags = 10
-  xrayIndex = true
+  key                = "docker-dev-local"
+  package_type       = "docker"
+  description        = "Repo for uploading images to for dev (non-prod environments)"
+  notes              = "This repo should be pruned to avoid maintaining old images."
+  docker_api_version = "V2"
+  max_unique_tags    = 10
 }
 
 resource "artifactory_local_repository" "docker_prod_local" {
-  key             = "docker-prod-local"
-  package_type    = "docker"
-  description     = "Repo for uploading images to for dev (non-prod environments)"
-  notes           = "This repo should be pruned to avoid maintaining old images."
-  dockerApiVersion = "V2"
-  maxUniqueTags = 0
-  xrayIndex = true
+  key                = "docker-prod-local"
+  package_type       = "docker"
+  description        = "Repo for uploading images to for dev (non-prod environments)"
+  notes              = "This repo should be pruned to avoid maintaining old images."
+  docker_api_version = "V2"
+  max_unique_tags    = 0
 }
 
 # Create a new Artifactory remote repository called my-remote
 # https://github.com/atlassian/terraform-provider-artifactory/blob/master/website/docs/r/artifactory_remote_repository.html.markdown
 resource "artifactory_remote_repository" "docker_remote" {
-  key             = "docker-remote"
-  package_type    = "docker"
-  url             = "https://registry-1.docker.io/"
-  description     = "Repo for proxy to docker repos"
-  repo_layout_ref = "docker-default"
-  enableTokenAuthentication = true
-  dockerApiVersion = "V2"
-  xrayIndex = true
+  key                         = "docker-remote"
+  package_type                = "docker"
+  url                         = "https://registry-1.docker.io/"
+  description                 = "Repo for proxy to docker repos"
+  repo_layout_ref             = "docker-default"
+  enable_token_authentication = true
 }
 
 # Create a new Artifactory remote repository called my-remote
 # https://github.com/atlassian/terraform-provider-artifactory/blob/master/website/docs/r/artifactory_virtual_repository.html.markdown
 resource "artifactory_virtual_repository" "docker-remote" {
-  key             = "docker-remote"
-  package_type    = "docker"
-  description     = "External repo for all images to be downloaded from."
-  defaultDeploymentRepo = "${artifactory_local_repository.docker_dev_local.key}"
+  key                     = "docker-remote"
+  package_type            = "docker"
+  description             = "External repo for all images to be downloaded from."
+  default_deployment_repo = "${artifactory_local_repository.docker_dev_local.key}"
   repositories = [
     "${artifactory_local_repository.docker_dev_local.key}",
     "${artifactory_local_repository.docker_prod_local.key}",
