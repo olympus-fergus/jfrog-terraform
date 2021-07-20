6dc19eb583f103060a9958c73747509ce2225f1a
Fergus MacDermot <fergusmacdermot@gmail.com>
2021-07-20 08:15:55 +0800

    Update to terraform 1

--- users.tf
+++ users.tf
@@ -1,7 +1,7 @@
 resource "artifactory_user" "developer_read_docker_all" {
   name              = "developer-read-docker-all"
   email             = "fergus.macdermot@gmail.com"
-  groups            = ["${artifactory_group.read_docker_all.name}"]
+  groups            = [artifactory_group.groups["read_docker_all"].name]
   admin             = false
   profile_updatable = false
   disable_ui_access = true
